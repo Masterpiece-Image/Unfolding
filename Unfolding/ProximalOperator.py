9f16e57d37339396c973b28144f8b37f38c61ab5
akhaten <khafifjessy.github@gmail.com>
2022-12-02 16:34:25 +0100

    rewrote with taking account "variable_scope"

--- Unfolding/ProximalOperator.py
+++ Unfolding/ProximalOperator.py
@@ -1,5 +1,5 @@
 import torch
-import torch.nn
+import torch.nn as nn
 import torch.nn.functional
 
 import typing
@@ -7,118 +7,44 @@ import typing
 class ProximalOperator(torch.nn.Module):
 
 
-    def __init__(self, in_channels: int, num_features: int) -> None:
+    def __init__(self, in_channels: int, num_features: int) -> None:    
+        super().__init__()   
 
-        """
-        :in_channels 
-        :num_features
-        """
-      
-        self.in_channels: int = in_channels
-        self.num_features: int = num_features
+        self.in_channels = in_channels
 
-        self.conv: torch.nn.Conv2d = torch.nn.Conv2d(
-            in_channels=self.in_channels,
-            out_channels=self.num_features,
-            kernel_size=(3, 3),
-            padding='same'
-        )
+        #scope prox
+        self.conv = nn.Conv2d(in_channels=in_channels, out_channels=num_features, kernel_size=3, padding="same")
+
+        self.loop_conv = {}
+        for i in range(5):
+            #scope iteration_i
+            self.loop_conv["conv"+str(i)+"1"] = nn.Conv2d(in_channels=num_features, out_channels=num_features, kernel_size=3, padding="same")
+            self.loop_conv["conv"+str(i)+"2"] = nn.Conv2d(in_channels=num_features, out_channels=num_features, kernel_size=3, padding="same")
+
+        self.conv3 = nn.Conv2d(in_channels=num_features, out_channels=in_channels, kernel_size=3, padding="same")
         
-        self.conv1: torch.nn.Conv2d = torch.nn.Conv2d(
-            in_channels=self.in_channels,
-            out_channels=self.num_features,
-            kernel_size=(3, 3),
-            padding='same'
-        )
-
-        self.conv2: torch.nn.Conv2d = torch.nn.Conv2d(
-            in_channels=self.in_channels,
-            out_channels=self.num_features,
-            kernel_size=(3, 3),
-            padding='same'
-        )
-
 
     def forward(self, image: torch.Tensor) -> torch.Tensor:
-
-        conv_out: torch.Tensor = self.conv(image)
-        conv_out: torch.Tensor = torch.nn.functional.relu(conv_out)
-
-        for _ in range(0, 5):
-            conv1_out: torch.Tensor = self.conv1(conv_out)
-            conv1_out: torch.Tensor = torch.nn.functional.relu(conv1_out)
-            conv2_out: torch.Tensor = self.conv2(conv1_out)
-            conv2_out: torch.Tensor = torch.nn.functional.relu(conv2_out)
-            conv_out: torch.Tensor = conv_out + conv2_out
-
-        return conv_out
+        conv = self.conv(image)
+        conv = nn.functional.relu(conv)
+        for i in range(5):
+            conv_1 = self.loop_conv["conv"+str(i)+"1"](conv)
+            conv_1 = nn.functional.relu(conv_1)
+            conv_2 = self.loop_conv["conv"+str(i)+"2"](conv_1)
+            conv_2 = nn.functional.relu(conv_2)
+            conv = conv_1 + conv_2
+        out = self.conv3(conv)
+        return out
 
 
 class Prox_M(ProximalOperator):
-
     def __init__(self, in_channels: int) -> None:
-        """
-        :in_channels
-        """
-        super().__init__(self, in_channels=in_channels, num_features=in_channels // 2)
+        super().__init__(in_channels=in_channels, num_features=in_channels // 2)
 
-    @classmethod
-    def from_image(image: torch.Tensor) -> 'Prox_M':
-        return Prox_M(in_channels=image.shape[-1])
-
-      
 
 class Prox_O(ProximalOperator):
-
-    def __init__(self, in_channels: int, num_features: int) -> None:
-        super().__init__(in_channels=in_channels, num_features=num_features)
-    
-    def forward(self, image: torch.Tensor) -> typing.Tuple[torch.Tensor, torch.Tensor]:
-        out: torch.Tensor = super().forward(image)
-        B: torch.Tensor = out[:, :, :, 0:1]
-        Z: torch.Tensor = out[:, :, :, 1: self.in_channels]
+    def forward(self, image : torch.Tensor) -> torch.Tensor:
+        out = super().forward(image)
+        B = out[:, 0:1, :, :]
+        Z = out[:, 1:self.in_channels, :, :]
         return B, Z
-
-    # @classmethod
-    # def from_image(image: torch.Tensor) -> 'Prox_M':
-    #     return Prox_M(in_channels=image.shape[-1], num_features=image.shape[-1]//2)
-
-
-
-
-# def prox_M(image: torch.Tensor) -> torch.Tensor:
-
-#     inchannels: int = image.shape[-1]
-#     num_features: int  =  inchannels // 2
-#     conv: torch.Tensor = torch.conv2d(image, num_features, 3, padding="same")
-#     conv: torch.Tensor = torch.nn.functional.relu(conv)
-
-#     for _ in range(5):
-#         conv_1: torch.Tensor = torch.conv2d(conv, num_features, 3, padding="same")
-#         conv_1: torch.Tensor = torch.nn.functional.relu(conv_1)
-#         conv_2: torch.Tensor = torch.conv2d(conv_1, num_features, 3, padding="same")
-#         conv_2: torch.Tensor = torch.nn.functional.relu(conv_2)
-#         conv: torch.Tensor = conv + conv_2
-
-#     out: torch.Tensor = torch.conv2d(conv, inchannels, 3, padding = 'same')
-
-#     return out
-
-# def prox_O(image: torch.Tensor, num_features: int) -> typing.Tuple[torch.Tensor, torch.Tensor]:        
-    
-#     inchannels: int = image.shape[-1]
-#     conv: torch.Tensor = torch.conv2d(image, num_features, 3, padding="same")
-#     conv = torch.nn.functional.relu(conv)
-    
-#     for i in range(5):
-#         conv_1: torch.Tensor = torch.conv2d(conv, num_features, 3, padding="same")
-#         conv_1: torch.Tensor = torch.nn.functional.relu(conv_1)
-#         conv_2: torch.Tensor = torch.conv2d(conv_1, num_features, 3, padding="same")
-#         conv_2: torch.Tensor = torch.nn.functional.relu(conv_2)
-#         conv: torch.Tensor = conv + conv_2
-
-#     out: torch.Tensor = torch.conv2d(conv, inchannels, 3, padding = 'same')
-#     B: torch.Tensor = out[:,:,:,0:1]
-#     Z: torch.Tensor = out[:,:,:,1:inchannels]
-
-#     return B, Z
--- Unfolding/Unfolding.py
+++ Unfolding/Unfolding.py
@@ -1,5 +1,5 @@
 import torch
-import torch.nn
+import torch.nn as nn
 # from torch.nn import L1Loss as MAE
 
 import typing
@@ -15,246 +15,104 @@ class Unfolding(torch.nn.Module):
 
 
     def __init__(self, in_channels: int, num_features: int = 48, iterations: int = 10) -> None:
+        super().__init__()
+
+        self.prox_O: ProximalOperator.Prox_O = ProximalOperator.Prox_O(in_channels=in_channels+num_features, num_features=num_features)
+        self.prox_M: ProximalOperator.Prox_M = ProximalOperator.Prox_M(in_channels=num_features*3)
+        self.stepO = torch.tensor(0.1, dtype=torch.double, requires_grad=True)
+        self.stepM = torch.tensor(0.1, dtype=torch.double, requires_grad=True)
+        self.num_features = num_features
+        self.iterations = iterations
+
+        #initial scope
+        self.O_0 = nn.Conv2d(in_channels=1, out_channels=num_features, kernel_size=3, padding="same")
+
+        #iteration scope
+        self.conv_X1 = nn.Conv2d(in_channels=in_channels, out_channels=num_features, kernel_size=3, dilation=1, padding="same", bias=False)
+        self.conv_X2 = nn.Conv2d(in_channels=in_channels, out_channels=num_features, kernel_size=3, dilation=2, padding="same", bias=False)
+        self.conv_X4 = nn.Conv2d(in_channels=in_channels, out_channels=num_features, kernel_size=3, dilation=4, padding="same", bias=False)
 
 
-        self.in_channels: int = in_channels
-        self.num_features: int = num_features
-        self.iterations: int = iterations
+        self.conv_X11 = nn.Conv2d(in_channels=num_features, out_channels=num_features, kernel_size=3, dilation=1, padding="same", bias=False)
+        self.conv_X22 = nn.Conv2d(in_channels=num_features, out_channels=num_features, kernel_size=3, dilation=2, padding="same", bias=False)
+        self.conv_X44 = nn.Conv2d(in_channels=num_features, out_channels=num_features, kernel_size=3, dilation=4, padding="same", bias=False)
 
-        # Convolutional layers
-        self.O_0: torch.nn.Conv2d = torch.nn.Conv2d(in_channels, out_channels=num_features, kernel_size=3)
-        self.X_1: torch.nn.Conv2d = torch.nn.Conv2d(in_channels, out_channels=num_features, kernel_size=3, dilation=(1, 1), padding='same', bias=False)
-        self.X_2: torch.nn.Conv2d = torch.nn.Conv2d(in_channels, out_channels=num_features, kernel_size=3, dilation=(2, 2), padding='same', bias=False)
-        self.X_4: torch.nn.Conv2d = torch.nn.Conv2d(in_channels, out_channels=num_features, kernel_size=3, dilation=(4, 4), padding='same', bias=False)
-
-        # Proximal operators
-        self.prox_M: ProximalOperator.Prox_M = ProximalOperator.Prox_M(in_channels=in_channels)
-        self.prox_O: ProximalOperator.Prox_O = ProximalOperator.Prox_O(in_channels=in_channels, num_features=num_features)
-
-        # Step tensors
-        self.stepO: torch.DoubleTensor = torch.DoubleTensor(0.1, requires_grad=True)
-        self.stepM: torch.DoubleTensor = torch.DoubleTensor(0.1, requires_grad=True)
+        self.conv_X111 = nn.Conv2d(in_channels=num_features, out_channels=num_features, kernel_size=3, dilation=1, padding="same", bias=False)
+        self.conv_X222 = nn.Conv2d(in_channels=num_features, out_channels=num_features, kernel_size=3, dilation=2, padding="same", bias=False)
+        self.conv_X444 = nn.Conv2d(in_channels=num_features, out_channels=num_features, kernel_size=3, dilation=4, padding="same", bias=False)
         
-        # Accumulator for save output for each stage
-        self.outputs_stage: list[torch.Tensor] = []
 
+        
 
     def forward(self, J: torch.Tensor) -> torch.Tensor:
+        #initial
+        O_0 = self.O_0(J)
+        a = [O_0,J]
+        tmp = torch.concat([O_0,J],1)
+        O_previous, Z = self.prox_O(tmp)
+        H = J - O_previous
 
-        nb_stage: int = self.iterations
+        #iteration 1
+        X_1 = self.conv_X1(H)
+        X_2 = self.conv_X2(H)
+        X_4 = self.conv_X4(H)
 
-        # Initialisation
-        O_0_out: torch.Tensor = self.O_0(J)
-        tmp: torch.Tensor = torch.concat([O_0_out, J], -1)
-        O_previous, Z : typing.Tuple[torch.Tensor, torch.Tensor] = self.prox_O(tmp)
-        H: torch.Tensor = J - O_previous
+        M = self.prox_M(torch.concat([X_1,X_2,X_4],1))
 
-        # self.outputs_stage((torch.tensor(O_current), torch.tensor(H_current))) ???????????????
+        X_1 = self.conv_X11(M[:,0:self.num_features,:,:])
+        X_2 = self.conv_X22(M[:,self.num_features:self.num_features*2,:,:])
+        X_4 = self.conv_X44(M[:,self.num_features*2:self.num_features*3,:,:])
 
-        # Stage 1
-        X_1_out: torch.Tensor = self.X_1(H)
-        X_2_out: torch.Tensor = self.X_2(H)           
-        X_4_out: torch.Tensor = self.X_4(H)            
-      
-        M: torch.Tensor = self.prox_M(torch.concat([X_1_out, X_2_out, X_4_out], -1))
-       
-        X_1_out: torch.Tensor = self.X_1(M[:, :, :, 0:self.num_features])
-        X_2_out: torch.Tensor = self.X_2(M[:, :, :, self.num_features:self.num_features*2])           
-        X_4_out: torch.Tensor = self.X_4(M[:, :, :, self.num_features*2:self.num_features*3])
-            
-        h_current: torch.Tensor =  torch.concat([X_1_out, X_2_out, X_4_out], -1)
-        H_current: torch.Tensor = h_current.sum(dim=-1, keepdim=True)
-       
-        O_current: torch.Tensor = J - H_current     
-        tmp: torch.Tensor = torch.concat([Z, self.stepO * O_current + (1. - self.stepO) * O_previous],-1)
-            
-        O_current, Z : typing.Tuple[torch.Tensor, torch.Tensor] = self.prox_O(tmp)
+        h_current = torch.concat([X_1,X_2,X_4],1)
+        H_current = h_current.sum(1)
 
-        self.outputs_stage((torch.tensor(O_current), torch.tensor(H_current)))
+        O_current = J - H_current
+        tmp = torch.concat([Z, self.stepO * O_current + (1.-self.stepO) * O_previous],1)
+        O_current, Z = self.prox_O(tmp)
+        print("passe 1")
 
-        # Stage 2...nb_stage
-        for _ in range(2, nb_stage):   # iterations 2 to 10                 
-            
-            O_previous = O_current       
+        for i in range(self.iterations-1):
+            O_previous = O_current
             H = J - O_previous
 
-            X_1_out: torch.Tensor = self.X_1(M[:, :, :, 0:self.num_features])
-            X_2_out: torch.Tensor = self.X_2(M[:, :, :, self.num_features:self.num_features*2])           
-            X_4_out: torch.Tensor = self.X_4(M[:, :, :, self.num_features*2:self.num_features*3])
-            
-            H_star: torch.Tensor = torch.concat([X_1_out, X_2_out, X_4_out], -1) 
-            H_star: torch.Tensor = H_star.sum(dim=-1, keepdim=True)
-            
-            X_1_out: torch.Tensor = self.X_1(H_star-H)  
-            X_2_out: torch.Tensor = self.X_2(H_star-H)            
-            X_4_out: torch.Tensor = self.X_4(H_star-H)               
+            X_1 = self.conv_X11(M[:,0:self.num_features,:,:])
+            X_2 = self.conv_X22(M[:,self.num_features:self.num_features*2,:,:])
+            X_4 = self.conv_X44(M[:,self.num_features*2:self.num_features*3,:,:])
 
-            M: torch.Tensor = self.prox_M(M - self.stepM * torch.concat([X_1_out, X_2_out, X_4_out], -1))
-            
-            X_1_out: torch.Tensor = self.X_1(M[:, :, :, 0:self.num_features])    
-            X_2_out: torch.Tensor = self.X_2(M[:, :, :, self.num_features:self.num_features*2])             
-            X_4_out: torch.Tensor = self.X_4(M[:, :, :, self.num_features*2:self.num_features*3])
-            
-            h_current: torch.Tensor = torch.concat([X_1_out, X_2_out, X_4_out], -1) 
-            H_current: torch.Tensor = h_current.sum(dim=-1, keepdim=True)
-            
-            O_current: torch.Tensor = J - H_current        
-            tmp: torch.Tensor = torch.concat([Z, self.stepO * O_current + (1. - self.stepO) * O_previous], -1)
-        
-            O_current, Z: typing.Tuple[torch.Tensor, torch.Tensor] = self.prox_O(tmp, self.num_features)
+            H_star = torch.concat([X_1,X_2,X_4],1)
+            H_star = H_star.sum(1)
 
-            self.outputs_stage((torch.tensor(O_current), torch.tensor(H_current)))
-                        
-        final_out: torch.Tensor = O_current
+            X_1 = self.conv_X1(H_star-H)
+            X_2 = self.conv_X2(H_star-H)
+            X_4 = self.conv_X4(H_star-H)
 
-        return final_out
+            M = self.prox_M(M - self.stepM * torch.concat([X_1,X_2,X_4],1))
 
-    # def forward(self, J: torch.Tensor, iterations: int = 10) -> torch.Tensor:
+            X_1 = self.conv_X111(M[:,0:self.num_features,:,:])
+            X_2 = self.conv_X222(M[:,self.num_features:self.num_features*2,:,:])
+            X_4 = self.conv_X444(M[:,self.num_features*2:self.num_features*3,:,:])
 
-    #     nb_stage: int = iterations
+            h_current = torch.concat([X_1,X_2,X_4],1)
+            H_current = h_current.sum(1)
 
-    #     # Initialisation
-    #     self.O_0: torch.Tensor = self.O_0(J)
-    #     tmp: torch.Tensor = torch.concat([self.O_0, J], -1)
-    #     O_previous, Z : typing.Tuple[torch.Tensor, torch.Tensor] = self.prox_O(tmp)
-    #     H: torch.Tensor = J - O_previous
+            O_current = J - H_current
+            tmp = torch.concat([Z, self.stepO * O_current + (1.-self.stepO) * O_previous],1)
+            O_current, Z = self.prox_O(tmp)
+            print("passe " + str(i))
 
-    #     # Stage 1
-    #     self.X_1: torch.Tensor = self.X_1(H)
-    #     self.X_2: torch.Tensor = self.X_2(H)           
-    #     self.X_4: torch.Tensor = self.X_4(H)            
-      
-    #     M: torch.Tensor = self.prox_M(torch.concat([self.X_1, self.X_2, self.X_4], -1))
-       
-    #     self.X_1: torch.Tensor = self.X_1(M[:, :, :, 0:self.num_features])
-    #     self.X_2: torch.Tensor = self.X_2(M[:, :, :, self.num_features:self.num_features*2])           
-    #     self.X_4: torch.Tensor = self.X_4(M[:, :, :, self.num_features*2:self.num_features*3])
-            
-    #     h_current: torch.Tensor =  torch.concat([self.X_1, self.X_2, self.X_4], -1)
-    #     H_current: torch.Tensor = h_current.sum(dim=-1, keepdim=True)
-       
-    #     O_current: torch.Tensor = J - H_current     
-    #     tmp: torch.Tensor = torch.concat([Z, self.stepO * O_current + (1. - self.stepO) * O_previous],-1)
-            
-    #     O_current, Z : typing.Tuple[torch.Tensor, torch.Tensor] = self.prox_O(tmp)
-
-    #     # Stage 2...nb_stage
-    #     for _ in range(2, nb_stage):   # iterations 2 to 10                 
-            
-    #         O_previous = O_current       
-    #         H = J - O_previous
-
-    #         self.X_1: torch.Tensor = self.X_1(M[:, :, :, 0:self.num_features])
-    #         self.X_2: torch.Tensor = self.X_2(M[:, :, :, self.num_features:self.num_features*2])           
-    #         self.X_4: torch.Tensor = self.X_4(M[:, :, :, self.num_features*2:self.num_features*3])
-            
-    #         H_star: torch.Tensor = torch.concat([self.X_1, self.X_2, self.X_4], -1) 
-    #         H_star: torch.Tensor = H_star.sum(dim=-1, keepdim=True)
-            
-    #         self.X_1: torch.Tensor = self.X_1(H_star-H)  
-    #         self.X_2: torch.Tensor = self.X_2(H_star-H)            
-    #         self.X_4: torch.Tensor = self.X_4(H_star-H)               
-
-    #         M: torch.Tensor = self.prox_M(M - self.stepM * torch.concat([self.X_1, self.X_2, self.X_4], -1))
-            
-    #         self.X_1: torch.Tensor = self.X_1(M[:, :, :, 0:self.num_features])
-    #         self.X_2: torch.Tensor = self.X_2(M[:, :, :, self.num_features:self.num_features*2])           
-    #         self.X_4: torch.Tensor = self.X_4(M[:, :, :, self.num_features*2:self.num_features*3])
-            
-    #         h_current: torch.Tensor = torch.concat([self.X_1, self.X_2, self.X_4], -1)
-    #         H_current: torch.Tensor = h_current.sum(dim=-1, keepdim=True)
-            
-    #         O_current: torch.Tensor = J - H_current
-    #         tmp: torch.Tensor = torch.concat([Z, self.stepO * O_current + (1. - self.stepO) * O_previous], -1)
-        
-    #         O_current, Z: typing.Tuple[torch.Tensor, torch.Tensor] = self.prox_O(tmp, self.num_features)
-
-    #         self.outputs_stage((torch.tensor(O_current), torch.tensor(H_current)))
-                        
-    #     final_out: torch.Tensor = O_current
-
-    #     return final_out
+        return O_current
 
 
-        
 
 
-    
-# class Unfolding(torch.nn.Module):
-
-#     def __init__(self) -> None:
-        
-#         # Accumulator for save output for each stage
-#         self.outputs_stage: list[torch.Tensor] = []
 
 
-#     def forward(self, J: torch.Tensor, num_features: int = 48, iterations: int = 10) -> torch.Tensor:
 
-#         nb_stage: int = iterations
 
-#         # Initialisation
-#         O_0: torch.Tensor = torch.conv2d(J, num_features, 3, padding='same')
-#         tmp: torch.Tensor = torch.concat([O_0, J], -1)
-#         O_previous, Z = ProximalOperator.prox_O(tmp, num_features)
-#         H = J - O_previous
 
-#         # Stage 1
-#         X_1 = torch.conv2d(H, num_features, 3, dilation_rate=(1, 1), padding="same", use_bias = False)
-#         X_2 = torch.conv2d(H, num_features, 3, dilation_rate=(2, 2), padding="same", use_bias = False)
-#         X_4 = torch.conv2d(H, num_features, 3, dilation_rate=(4, 4), padding="same", use_bias = False)
-      
-#         M = ProximalOperator.prox_M(torch.concat([X_1, X_2, X_4],-1))
-       
-#         X_1 = torch.conv2d(M[:,:,:,0:num_features], num_features, 3, dilation_rate=(1, 1), padding="same", use_bias = False)    
-#         X_2 = torch.conv2d(M[:,:,:,num_features:num_features*2], num_features, 3, dilation_rate=(2, 2), padding="same", use_bias = False)             
-#         X_4 = torch.conv2d(M[:,:,:,num_features*2:num_features*3], num_features, 3, dilation_rate=(4, 4), padding="same", use_bias = False)
-            
-#         h_current =  torch.concat([X_1,X_2,X_4],-1)
-#         H_current: torch.Tensor = h_current.sum(dim=-1, keepdim=True)
-       
-#         O_current = J - H_current     
-#         stepO = torch.DoubleTensor(0.1, requires_grad=True)
-#         tmp = torch.concat([Z, stepO * O_current + (1. - stepO) * O_previous],-1)
-            
-#         O_current, Z = ProximalOperator.prox_O(tmp, num_features)
 
-#         # Stage 2...nb_stage
-#         for _ in range(2, nb_stage):   # iterations 2 to 10                 
-            
-#             O_previous = O_current       
-#             H = J - O_previous
 
-#             X_1 = torch.conv2d(M[:,:,:,0:num_features], num_features, 3, dilation_rate=(1, 1), padding="same", use_bias = False)  
-#             X_2 = torch.conv2d(M[:,:,:,num_features:num_features*2], num_features, 3, dilation_rate=(2, 2), padding="same", use_bias = False)           
-#             X_4 = torch.conv2d(M[:,:,:,num_features*2:num_features*3], num_features, 3, dilation_rate=(4, 4), padding="same", use_bias = False)
-            
-#             H_star = torch.concat([X_1,X_2,X_4],-1) 
-#             H_star = H_star.sum(dim=-1, keepdim=True)
-                
-#             X_1 = torch.conv2d(H_star-H, num_features, 3, dilation_rate=(1, 1), padding="same", use_bias = False)  
-#             X_2 = torch.conv2d(H_star-H, num_features, 3, dilation_rate=(2, 2), padding="same", use_bias = False)            
-#             X_4 = torch.conv2d(H_star-H, num_features, 3, dilation_rate=(4, 4), padding="same", use_bias = False)               
 
-#             stepM = torch.DoubleTensor(0.1, requires_grad=True)                  
-#             M = ProximalOperator.prox_M(M - stepM * torch.concat([X_1, X_2, X_4],-1))
-            
-#             X_1 = torch.conv2d(M[:,:,:,0:num_features], num_features, 3, dilation_rate=(1, 1), padding="same", use_bias = False)    
-#             X_2 = torch.conv2d(M[:,:,:,num_features:num_features*2], num_features, 3, dilation_rate=(2, 2), padding="same", use_bias = False)             
-#             X_4 = torch.conv2d(M[:,:,:,num_features*2:num_features*3], num_features, 3, dilation_rate=(4, 4), padding="same", use_bias = False)
-            
-#             h_current = torch.concat([X_1,X_2,X_4],-1) 
-#             H_current = h_current.sum(dim=-1, keepdim=True)
-            
-#             O_current = J - H_current        
-#             stepO = torch.DoubleTensor(0.1, requires_grad=True)
-#             tmp = torch.concat([Z, stepO * O_current + (1. - stepO) * O_previous],-1)
-        
-#             O_current, Z = ProximalOperator.prox_O(tmp, num_features)
 
-#             self.outputs_stage((torch.tensor(O_current), torch.tensor(H_current)))
-                        
-#         final_out = O_current
 
-#         return final_out
 
-   
